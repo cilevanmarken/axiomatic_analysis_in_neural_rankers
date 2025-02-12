34da816f691a68370893fc26dd251e4602522fb0
Cile van Marken <cilevanmarken@gmail.com>
2025-02-12 10:38:46 +0100

    Reformatted helpers

--- helpers.py
+++ helpers.py
@@ -13,17 +13,20 @@ from itertools import product
 
 # Function to load JSON file into a Python dictionary
 def load_json_file(file_path):
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         # Load JSON data into a dictionary
         data = json.load(file)
     return data
+
 
 def load_tokenizer_and_models(hf_model_name, device):
     tokenizer = AutoTokenizer.from_pretrained(hf_model_name)
     hf_model = AutoModel.from_pretrained(hf_model_name)
     hf_model.to(device)
 
-    tl_model = HookedEncoder.from_pretrained(hf_model_name, device=device, hf_model=hf_model)
+    tl_model = HookedEncoder.from_pretrained(
+        hf_model_name, device=device, hf_model=hf_model
+    )
 
     return tokenizer, hf_model, tl_model
 
@@ -33,16 +36,20 @@ def preprocess(dataset, tokenizer, remove_cols=["title", "text"]):
         return tokenizer(inputs["text"], truncation=True)
 
     tokenized_dataset = dataset.cast_column("_id", Value(dtype="int32"))
-    tokenized_dataset = dataset.map(tokenize_fn, batched=True, remove_columns=remove_cols)
+    tokenized_dataset = dataset.map(
+        tokenize_fn, batched=True, remove_columns=remove_cols
+    )
 
     # Convert to torch tensors
-    tokenized_dataset = tokenized_dataset.with_format("torch", columns=["input_ids", "attention_mask", "_id"])
+    tokenized_dataset = tokenized_dataset.with_format(
+        "torch", columns=["input_ids", "attention_mask", "_id"]
+    )
 
     return tokenized_dataset
 
 
 def preprocess_queries(queries_df, tokenizer):
-    dataset = Dataset.from_pandas(queries_df)    
+    dataset = Dataset.from_pandas(queries_df)
     tokenized_dataset = preprocess(dataset, tokenizer, remove_cols=["text"])
     dataloader = DataLoader(tokenized_dataset, batch_size=1)
 
@@ -59,41 +66,45 @@ def preprocess_corpus(corpus_dict, tokenizer):
     return dataloader
 
 
-'''
+"""
 Encoding loop for Huggingface models
-'''
+"""
+
+
 def encode_hf(model, dataloader, device):
-  result = np.empty((0,768))
-  all_labels = np.empty(0)
-
-  # send batch to device
-  for i, batch in enumerate(tqdm(dataloader)):
-    labels = batch.pop("_id")
-    batch = {k: v.to(device) for k, v in batch.items()}
-
-    # set model to eval and disable gradient calculations
-    model.eval()
-    with torch.no_grad():
-      # [0] selects the hidden states
-      # [:,0,:] selects the CLS token for each vector
-      outputs = model(**batch)[0][:,0,:].squeeze(0)
-
-    embeddings = outputs.detach().cpu().numpy()
-    # labels = batch["_ids"].detach().cpu().numpy()
-
-    result = np.concatenate((result, embeddings), axis=0)
-    all_labels = np.concatenate((all_labels, labels))
-
-  return result, np.asarray(all_labels)
-
-
-'''
-Encoding loop for TransformerLens models
-'''
-def encode_tl(model, dataloader):
-    result = np.empty((0,768))
+    result = np.empty((0, 768))
     all_labels = np.empty(0)
-   
+
+    # send batch to device
+    for i, batch in enumerate(tqdm(dataloader)):
+        labels = batch.pop("_id")
+        batch = {k: v.to(device) for k, v in batch.items()}
+
+        # set model to eval and disable gradient calculations
+        model.eval()
+        with torch.no_grad():
+            # [0] selects the hidden states
+            # [:,0,:] selects the CLS token for each vector
+            outputs = model(**batch)[0][:, 0, :].squeeze(0)
+
+        embeddings = outputs.detach().cpu().numpy()
+        # labels = batch["_ids"].detach().cpu().numpy()
+
+        result = np.concatenate((result, embeddings), axis=0)
+        all_labels = np.concatenate((all_labels, labels))
+
+    return result, np.asarray(all_labels)
+
+
+"""
+Encoding loop for TransformerLens models
+"""
+
+
+def encode_tl(model, dataloader):
+    result = np.empty((0, 768))
+    all_labels = np.empty(0)
+
     for _, batch in enumerate(tqdm(dataloader)):
         labels = batch.pop("_id")
 
@@ -101,12 +112,13 @@ def encode_tl(model, dataloader):
         input_ids = batch.get("input_ids")
         attn_mask = batch.get("attention_mask")
 
-        outputs = model(input_ids, return_type="embeddings", one_zero_attention_mask=attn_mask)
-        embeddings = outputs[:,0,:].squeeze(0).detach().cpu().numpy()
+        outputs = model(
+            input_ids, return_type="embeddings", one_zero_attention_mask=attn_mask
+        )
+        embeddings = outputs[:, 0, :].squeeze(0).detach().cpu().numpy()
 
         result = np.concatenate((result, embeddings), axis=0)
         all_labels = np.concatenate((all_labels, labels))
-
 
     return result, np.asarray(all_labels)
 
@@ -114,7 +126,7 @@ def encode_tl(model, dataloader):
 # Compute ranking scores using dot product
 def compute_ranking_scores(query_embedding, doc_embeddings, doc_ids):
     scores = np.matmul(query_embedding, doc_embeddings.T)
-    
+
     # sort scores and labels
     sorted_idx = np.argsort(scores)[::-1]
 
@@ -128,19 +140,24 @@ def create_df_from_nested_dict(nested_dict):
         for outer_id, inner_dict in nested_dict.items():
             inner_ids = list(set(inner_dict.keys()))
             combinations.extend(list(product([outer_id], inner_ids)))
-        
-        df = pd.DataFrame(combinations, columns=['qid', 'doc_id'], dtype=str)
-        df.sort_values(by=['qid', 'doc_id'], inplace=True)
-        
+
+        df = pd.DataFrame(combinations, columns=["qid", "doc_id"], dtype=str)
+        df.sort_values(by=["qid", "doc_id"], inplace=True)
+
         # Add empty columns for future scoring
         score_columns = [
-            'og_score', 'p_score', 'score_diff',
-            'percent_change', 'og_rank', 'p_rank', 'change_in_rank'
+            "og_score",
+            "p_score",
+            "score_diff",
+            "percent_change",
+            "og_rank",
+            "p_rank",
+            "change_in_rank",
         ]
-        
+
         for col in score_columns:
             df[col] = pd.NA
-        
+
         return df
 
     except Exception as e:
